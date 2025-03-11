4477e46e276e4efc6fd5fd9d3d3384cd70aefa3a
WhyAre <laiyr52@gmail.com>
2025-03-11 17:08:22 +0800

    Remove unused function

--- src/main/java/seedu/address/logic/parser/DeleteTutorialCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteTutorialCommandParser.java
@@ -3,7 +3,6 @@ package seedu.address.logic.parser;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import java.util.Objects;
-import java.util.stream.Stream;
 
 import seedu.address.logic.commands.DeleteTutorialCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -13,14 +12,6 @@ import seedu.address.model.tutorial.Tutorial;
  * Parses input arguments and creates a new {@link DeleteTutorialCommand} object
  */
 public class DeleteTutorialCommandParser implements Parser<DeleteTutorialCommand> {
-
-    /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values
-     * in the given {@code ArgumentMultimap}.
-     */
-    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
-    }
 
     /**
      * Parses the given {@code String} of arguments in the context of the
